416f55fc73a6da6164d93a60e7680396c9f3565b
javila35 <josepharthuravila@gmail.com>
2020-11-14 10:59:04 -0800

    Finished site functionality.

--- src/index.js
+++ src/index.js
@@ -6,6 +6,19 @@ let button = document.getElementById('submit')
 
 
 button.addEventListener("click", function() {
+    header.innerHTML = textValue.value
+    header.style.color = colorOption.value
     console.log(textValue.value)
     console.log(colorOption.value)
 })
+
+
+/*
+
+Next steps:
+    - Set up driver environment
+    - Write step defs
+    - Implement step def fuctionality
+    - Write article breaking it down
+
+*/
